c1d0477cf037511051fa123eee4e989c28f28b55
tithi13 <tithi.malkar@gmail.com>
2025-07-30 05:08:06 -0500

    Positive Negative in Java

---
+++ Posneg.java
@@ -0,0 +1,17 @@
+import java. util.*;
+public class Posneg
+{
+public static void main(String args[])
+{
+int n;
+Scanner s=new Scanner (System.in);
+System.out.println("Enter the no. you want to check");
+n=s.nextInt();
+if(n>0)
+{
+System.out.println("Number is Postive");
+}else{
+System.out.print("Number is negative");
+}
+}
+}
