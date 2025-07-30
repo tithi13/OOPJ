91216f552ed964dc4d7bdef0cc72f1d4850f6d18
tithi13 <tithi.malkar@gmail.com>
2025-07-30 05:03:14 -0500

    Addition program in java

---
+++ Addition1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Addition1
+{
+public static void main(String args[])
+{
+int a,b,c;
+Scanner sb=new Scanner(System.in);
+System.out.println("Enter the first no:");
+a=sb.nextInt();
+System.out.println("Enter the second no:");
+b=sb.nextInt();
+c=a+b;
+System.out.println("Addition of two no:"+c);
+}
+}
+ 
