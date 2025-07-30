3b06f609153b21febbab705f2ce787db79d5b843
tithi13 <tithi.malkar@gmail.com>
2025-07-30 05:07:12 -0500

    Greatest Number in Java

---
+++ Greatest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Greatest
+{
+  public static void main(String args[])
+{ 
+ int a,b;
+ Scanner s=new Scanner(System.in);
+ System.out.println("Enter a");
+ a=s.nextInt();
+ System.out.println("Enter b");
+ b=s.nextInt();
+ if(a<b){
+System.out.println("b is greater than a");
+}else{
+System.out.println("a is greater than b");
+}
+}
+}
+  
